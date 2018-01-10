f39d7bfa65dce26f8ea3dbeee6bb31bbbfa7922c
dulanjaleefl <dulanjalee.fl@yahoo.com>
2018-01-10 01:32:24 -0500

    3

---
+++ 12-creating-server/app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    console.log('the server request is at :' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey World');
+});
+
+server.listen(3001, '127.0.0.1');
+console.log('yo dawgs , yall are listening on port 3001');
